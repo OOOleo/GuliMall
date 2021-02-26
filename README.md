f36d756569239e1a4aceb59eeb82fdad3f518651
JK Oleo <46321645+oooleo@users.noreply.github.com>
2021-02-26 11:14:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GuliMall
+商城
---
+++ gulimall-order/src/test/java/com/leo/gulimall/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.gulimall.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
